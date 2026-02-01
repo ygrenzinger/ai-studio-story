d07354948d92b598a81c587d46905ffceea357d3
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2026-02-01 20:51:09 +0100

    chore: removing old generated codes

--- gemini_tts/services/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Services for TTS and audio conversion."""
-
-from gemini_tts.services.tts_service import GeminiTextToSpeechService
-from gemini_tts.services.audio_converter import AudioConverterService
-
-__all__ = ["GeminiTextToSpeechService", "AudioConverterService"]
--- gemini_tts/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions."""
-
-from gemini_tts.utils.wav_utils import create_wav_header, calculate_duration, format_duration
-
-__all__ = ["create_wav_header", "calculate_duration", "format_duration"]
